843ba1e47c73524dd910ef9bc4b1dc345d89c91b
TwistedArc <sbarkerla@gmail.com>
2019-05-29 15:43:24 -0700

    added pagination to datatable

--- src/app/training/past-training/past-training.component.ts
+++ src/app/training/past-training/past-training.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild, AfterViewInit } from "@angular/core";
-import { MatTableDataSource, MatSort } from "@angular/material";
+import { MatTableDataSource, MatSort, MatPaginator } from "@angular/material";
 import { Exercise } from "../exercise.model";
 import { ExerciseService } from "../exercise.service";
 
@@ -13,6 +13,7 @@ export class PastTrainingComponent implements OnInit, AfterViewInit {
   dataSource = new MatTableDataSource<Exercise>();
 
   @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor(private exerciseService: ExerciseService) {}
 
@@ -21,6 +22,7 @@ export class PastTrainingComponent implements OnInit, AfterViewInit {
   }
   ngAfterViewInit() {
     this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
   }
 
   doFilter(filterValue: string) {
