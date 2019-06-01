2c393559c2ca75130ae378802bdb1d8bdc8daba2
TwistedArc <sbarkerla@gmail.com>
2019-05-31 22:46:40 -0700

    setup envirn to ng build --prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDzvATTMjxbV5xsqz-jSkk5xnN6MntfSXU",
+    authDomain: "fitness-app-4d587.firebaseapp.com",
+    databaseURL: "https://fitness-app-4d587.firebaseio.com",
+    projectId: "fitness-app-4d587",
+    storageBucket: "fitness-app-4d587.appspot.com",
+    messagingSenderId: "730816159268"
+  }
 };
